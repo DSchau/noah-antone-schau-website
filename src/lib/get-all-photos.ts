92686f46523de5f868c5ce4344c1606f9db440b6
Dustin Schau <dustinschau@gmail.com>
2023-07-10 20:25:15 -0700

    chore: add blurhash... not sure I did this right tho

--- src/lib/get-all-photos.ts
+++ src/lib/get-all-photos.ts
@@ -1,6 +1,6 @@
 import { imgix } from './imgix'
 
-export async function getAllPhotos(): Promise<string[]> {
+export async function getAllPhotos(): Promise<any[]> {
   const response = await imgix(`/assets/{{source_id}}?page[number]=0&page[size]=100`)
     .then(res => res.data)
 
